2d811bd6e1dec8d38d0c78ca54e7f27e4c4039c7
Tobias Kopelke <kopelke@gmail.com>
2022-01-13 20:52:18 +0100

    removed react-icons and replaced them with svg versions

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,2 +1,89 @@
-export { FaChevronRight, FaDownload, FaTimes } from 'react-icons/fa'
-export { IoMdInformationCircleOutline } from 'react-icons/io'
+export const FaChevronRight = (props) => (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="chevron-right"
+    class="svg-inline--fa fa-chevron-right fa-w-10"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 320 512"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+    ></path>
+  </svg>
+)
+
+export const FaDownload = (props) => (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="download"
+    class="svg-inline--fa fa-download fa-w-16"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+    ></path>
+  </svg>
+)
+export const FaTimes = (props) => (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="times"
+    class="svg-inline--fa fa-times fa-w-11"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 352 512"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+    ></path>
+  </svg>
+)
+export const IoMdInformationCircleOutline = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="ionicon"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <title>Information Circle</title>
+    <path
+      d="M248 64C146.39 64 64 146.39 64 248s82.39 184 184 184 184-82.39 184-184S349.61 64 248 64z"
+      fill="none"
+      stroke="currentColor"
+      stroke-miterlimit="10"
+      stroke-width="32"
+    />
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="32"
+      d="M220 220h32v116"
+    />
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-miterlimit="10"
+      stroke-width="32"
+      d="M208 340h88"
+    />
+    <path d="M248 130a26 26 0 1026 26 26 26 0 00-26-26z" />
+  </svg>
+)
